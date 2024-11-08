722014e64a8d2666875d886a4fc651afda21f8c8
Charles Feval <fevalc>
2024-11-07 19:34:50 -0800

    Remove hotkeys

--- src/Commands/CompleteLineCommand.ts
+++ src/Commands/CompleteLineCommand.ts
@@ -2,26 +2,24 @@ import { LineOperations } from "../domain/LineOperations";
 import { Command, Editor, Hotkey, MarkdownView } from "obsidian";
 
 export class CompleteLineCommand implements Command {
-  constructor(private lineOperations: LineOperations) { }
+	constructor(private lineOperations: LineOperations) {}
 
-  id: string = "pw.complete-line";
-  name: string = "Complete line attributes";
-  icon?: string = "check-small";
-  mobileOnly?: boolean = false;
-  callback?: () => any;
-  checkCallback?: (checking: boolean) => boolean | void;
-  editorCallback(editor: Editor, view: MarkdownView) {
-    const lineNumber = editor.getCursor("from").line;
-    let line = editor.getLine(lineNumber);
-    line = this.lineOperations.convertAttributes(line);
-    editor.setLine(lineNumber, line);
-  };
-  editorCheckCallback?: (checking: boolean, editor: Editor, view: MarkdownView) => boolean | void;
-  hotkeys?: Hotkey[] = [
-    {
-      key: ".",
-      modifiers: ["Alt"]
-    }
-  ];
-
+	id: string = "pw.complete-line";
+	name: string = "Complete line attributes";
+	icon?: string = "check-small";
+	mobileOnly?: boolean = false;
+	callback?: () => any;
+	checkCallback?: (checking: boolean) => boolean | void;
+	editorCallback(editor: Editor, view: MarkdownView) {
+		const lineNumber = editor.getCursor("from").line;
+		let line = editor.getLine(lineNumber);
+		line = this.lineOperations.convertAttributes(line);
+		editor.setLine(lineNumber, line);
+	}
+	editorCheckCallback?: (
+		checking: boolean,
+		editor: Editor,
+		view: MarkdownView
+	) => boolean | void;
+	hotkeys?: Hotkey[] = [];
 }
--- src/Commands/ToggleOngoingTodoCommand.ts
+++ src/Commands/ToggleOngoingTodoCommand.ts
@@ -3,29 +3,30 @@ import { TodoStatus } from "../domain/TodoItem";
 import { Command, Editor, Hotkey, MarkdownView } from "obsidian";
 
 export class ToggleOngoingTodoCommand implements Command {
-  constructor(private lineOperations: LineOperations) { }
+	constructor(private lineOperations: LineOperations) {}
 
-  id: string = "pw.toggle-ongoing-todo-command";
-  name: string = "Mark todo as ongoing / unchecked";
-  icon?: string = "check-small";
-  mobileOnly?: boolean = false;
-  callback?: () => any;
-  checkCallback?: (checking: boolean) => boolean | void;
-  editorCallback(editor: Editor, view: MarkdownView) {
-    const lineNumber = editor.getCursor("from").line
-    let line = editor.getLine(lineNumber)
-    const todo = this.lineOperations.toTodo(line, lineNumber)
-    if (todo.isTodo) {
-      line = this.lineOperations.setCheckmark(line, todo.todo.status === TodoStatus.InProgress ? " " : ">")
-      editor.setLine(lineNumber, line)
-    }
-  };
-  editorCheckCallback?: (checking: boolean, editor: Editor, view: MarkdownView) => boolean | void;
-  hotkeys?: Hotkey[] = [
-    {
-      key: ">",
-      modifiers: ["Alt"]
-    }
-  ];
-
+	id: string = "pw.toggle-ongoing-todo-command";
+	name: string = "Mark todo as ongoing / unchecked";
+	icon?: string = "check-small";
+	mobileOnly?: boolean = false;
+	callback?: () => any;
+	checkCallback?: (checking: boolean) => boolean | void;
+	editorCallback(editor: Editor, view: MarkdownView) {
+		const lineNumber = editor.getCursor("from").line;
+		let line = editor.getLine(lineNumber);
+		const todo = this.lineOperations.toTodo(line, lineNumber);
+		if (todo.isTodo) {
+			line = this.lineOperations.setCheckmark(
+				line,
+				todo.todo.status === TodoStatus.InProgress ? " " : ">"
+			);
+			editor.setLine(lineNumber, line);
+		}
+	}
+	editorCheckCallback?: (
+		checking: boolean,
+		editor: Editor,
+		view: MarkdownView
+	) => boolean | void;
+	hotkeys?: Hotkey[] = [];
 }
