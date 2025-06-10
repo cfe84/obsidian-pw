103bbf64e2727bc104f647e2651d222d3a2e5560
Charles Feval <fevalc>
2025-06-10 08:19:18 -0700

    Fix #31: Escape search regex

--- src/domain/TodoMatcher.ts
+++ src/domain/TodoMatcher.ts
@@ -1,40 +1,50 @@
 import { TodoItem } from "./TodoItem";
 
 export class TodoMatcher<T> {
-  private matchTerm: string
-  private regex: RegExp
-  constructor(matchTerm: string, private fuzzySearch = false) {
-    this.matchTerm = matchTerm.toLowerCase()
-    this.matches = this.matches.bind(this)
-    this.regex = RegExp(matchTerm.replace(/ /g, ""), "gi")
-  }
+	private matchTerm: string;
+	private regex: RegExp;
+	constructor(matchTerm: string, private fuzzySearch = false) {
+		this.matchTerm = matchTerm.toLowerCase();
+		this.matches = this.matches.bind(this);
+		this.regex = RegExp(
+			matchTerm
+				.replace(/\\/g, "\\\\")
+				.replace(/\//g, "\\/")
+				.replace(/ /g, ""),
+			"gi"
+		);
+	}
 
-  public matches(todo: TodoItem<T>): boolean {
+	public matches(todo: TodoItem<T>): boolean {
+		if (!this.matchTerm) {
+			return true;
+		}
+		if (this.fuzzySearch) {
+			return this.fuzzyMatch(todo);
+		} else {
+			return this.exactMatch(todo);
+		}
+	}
 
-    if (!this.matchTerm) {
-      return true;
-    }
-    if (this.fuzzySearch) {
-      return this.fuzzyMatch(todo)
-    } else {
-      return this.exactMatch(todo)
-    }
-  }
+	private exactMatch(todo: TodoItem<T>) {
+		try {
+			return todo.text.search(this.regex) >= 0;
+		} catch (e) {
+			console.error("Error in regex matching:", e);
+			return false;
+		}
+	}
 
-  private exactMatch(todo: TodoItem<T>) {
-    return todo.text.search(this.regex) >= 0
-  }
-
-  private fuzzyMatch(todo: TodoItem<T>) {
-    let i = 0;
-    const todoText = todo.text.toLowerCase()
-    for (let char of this.matchTerm) {
-      let matchIndex = todoText.indexOf(char, i)
-      if (matchIndex < 0) {
-        return false
-      }
-      i = matchIndex
-    }
-    return true;
-  }
+	private fuzzyMatch(todo: TodoItem<T>) {
+		let i = 0;
+		const todoText = todo.text.toLowerCase();
+		for (let char of this.matchTerm) {
+			let matchIndex = todoText.indexOf(char, i);
+			if (matchIndex < 0) {
+				return false;
+			}
+			i = matchIndex;
+		}
+		return true;
+	}
 }
